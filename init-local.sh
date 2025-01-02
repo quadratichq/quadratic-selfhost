09b347656a4c2747c409f83b27a18ed355f33ac0
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-01-02 15:50:19 +0530

    remove get-host in init-local

--- init-local.sh
+++ init-local.sh
@@ -4,25 +4,14 @@
 # 
 # Usage:
 # 
-#   ./init.sh 83f0ebdf-eafb-4c8d-bd7b-04ea07d61b7f localhost
-# 
-# 
-# Flow:
-# 
-# First, check to see if there is a VERSION file, if so, use that version.
-# If not, then check for the first command line argument, if so, use that version.
-# Else, prompt the user.
-# 
-# First, check to see if there is a HOST file, if so, use that host.
-# If not, then check for the first command line argument, if so, use that host.
-# Else, prompt the user.
+#   ./init.sh 83f0ebdf-eafb-4c8d-bd7b-04ea07d61b7f
+
 
 REPO="https://github.com/quadratichq/quadratic-selfhost.git"
 SELF_HOSTING_URI="https://selfhost.quadratichq.com/"
 INVALID_LICENSE_KEY="Invalid license key."
 PROFILE=""
 LICENSE_KEY=""
-HOST=""
 
 cp docker/ory-auth/config/kratos.local.yml docker/ory-auth/config/kratos.yml
 cp .env.local .env
@@ -36,13 +25,6 @@ get_license_key() {
       echo $INVALID_LICENSE_KEY
       return 1
     fi
-}
-
-get_host() {
-    read -p "What public host name or public IP address are you using for this setup (e.g. localhost, app.quadratic.com, or other): " user_input
-    
-    # TODO: validate host
-    echo $user_input
 }
 
 parse_profile() {
@@ -96,14 +78,6 @@ else
   LICENSE_KEY=$(get_license_key)
 fi
 
-if [ -f "quadratic-selfhost/HOST" ]; then
-  HOST=$(<quadratic-selfhost/HOST)
-elif [ $2 ]; then
-  HOST=$2
-else
-  HOST=$(get_host)
-fi
-
 # retrieve the code from github
 checkout
 
@@ -115,10 +89,6 @@ echo $LICENSE_KEY > LICENSE_KEY
 PROFILE=$(parse_profile)
 touch PROFILE
 echo $PROFILE > PROFILE
-
-# write host to HOST file
-touch HOST
-echo $HOST > HOST
 
 # remove the init.sh script
 rm ../init.sh
